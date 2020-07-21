904da46430d5a6181908aa4071e96643419af116
Stephanie Cheney <steph@scheneyphotography.com>
2020-07-21 11:28:28 -0500

    linked parents to navMap

--- my-project-client/src/NavMap.js
+++ my-project-client/src/NavMap.js
@@ -14,7 +14,7 @@ class NavMap extends React.Component{
       <Rotate triggerOnce>
          <div id="bg">
           
-            <img src={require("./images/navMapImage.png")} usemap="#image-map"/>
+            <img src={require("./images/navMapImage2.png")} usemap="#image-map"/>
             <map name="image-map">
                 <Link to={{
                   pathname: "/levelone",
@@ -28,6 +28,12 @@ class NavMap extends React.Component{
                       currentChild: this.props.location.aboutProps.currentChild
                   }}
                   }><area coords="1150,1072,1265,1182" shape="rect"></area></Link>
+                    <Link to={{
+                  pathname: "/parents",
+                  aboutProps:{
+                      currentChild: this.props.location.aboutProps.currentChild
+                  }}
+                  }><area coords="89,1087,299,1250" shape="rect"></area></Link>
             </map>
         </div>
       </Rotate>
